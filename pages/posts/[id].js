4341909005d6fef96116dd0491a688d49ecbbd29
Stephen Price <stephen_lprice@live.com>
2021-07-05 01:19:35 -0500

    dynamic routes implemented getStaticProps()

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,37 @@
 import Layout from '../../components/layout'
-import getAllPostIds from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
+// files named inside brackets [] create dynamic routes, 
+// Next will create individual HTML routes for each .md file
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br/>
+      {postData.id}
+      <br/>
+      {postData.date}
+    </Layout>
+  )
+}
+
+// get dynamic paths using the id parameter defined in /lib/posts (.md file names)
 export async function getStaticPaths() {
-  const paths = getAllPostIds;
+  const paths = getAllPostIds();
+
+  console.log(paths)
   return {
     paths,
     fallback: false,
   }
 }
 
-export default function Post() {
-  return (
-    <Layout></Layout>
-  )
+// get post data for the corresponding .md file by id parameter
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData
+    }
+  }
 }
